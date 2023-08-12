80c1192eb459280187242e6e4bc1102006a272e3
Ishmum <ishmum20000@gmail.com>
2023-08-13 00:23:02 +0800

    Add routes

--- src/App.js
+++ src/App.js
@@ -3,27 +3,37 @@ import "./App.css";
 import SideBar from "./components/sidebar/sidebar/sidebar";
 import { createTheme, colors } from "@mui/material";
 import { ThemeProvider } from "@emotion/react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./components/login/login";
 
 const theme = createTheme({
-  typography: {
-    fontFamily: ["Inter"].join(","),
-    fontSize: 12,
-  },
-  // palette: {
-  //   divider: {
-  //     background: colors.orange[500],
-  //   },
-  // },
+    typography: {
+        fontFamily: ["Inter"].join(","),
+        fontSize: 12,
+    },
+    // palette: {
+    //   divider: {
+    //     background: colors.orange[500],
+    //   },
+    // },
 });
 
 function App() {
-  return (
-    <ThemeProvider theme={theme}>
-      <div className="App">
-        <SideBar />
-      </div>
-    </ThemeProvider>
-  );
+    return (
+        <div>
+            <Router>
+                <Routes>
+                    <Route path="/login" element={<Login></Login>}></Route>
+                </Routes>
+            </Router>
+
+            <ThemeProvider theme={theme}>
+                <div className="App">
+                    {/* <SideBar /> */}
+                </div>
+            </ThemeProvider>
+        </div>
+    );
 }
 
 export default App;
