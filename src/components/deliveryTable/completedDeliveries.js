d1201d14c6d92b45de19de07e0fa7d386dff765f
Ishmum <ishmum20000@gmail.com>
2023-08-14 15:32:30 +0800

    Add dummy deliveries data

---
+++ src/components/deliveryTable/completedDeliveries.js
@@ -0,0 +1,143 @@
+const completedDeliveries = () => {
+  const completedDeliveries = [
+    {
+      user: {
+        name: "Alice",
+        email: "alice@example.com",
+        address: "Address 42"
+      },
+      restaurant: "Kafe Selasih, Arked Cengal",
+      deliveryDate: "2023-08-01T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Bob",
+        email: "bob@example.com",
+        address: "Address 15"
+      },
+      restaurant: "Deen Corner, Arked Cengal",
+      deliveryDate: "2023-07-18T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Charlie",
+        email: "charlie@example.com",
+        address: "Address 73"
+      },
+      restaurant: "Belanga Cafe, Arked Cengal",
+      deliveryDate: "2023-04-05T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "David",
+        email: "david@example.com",
+        address: "Address 28"
+      },
+      restaurant: "The Kave, Arked Cengal",
+      deliveryDate: "2023-06-11T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Emma",
+        email: "emma@example.com",
+        address: "Address 57"
+      },
+      restaurant: "PUDJ, Arked N24 dan Arked KDSE",
+      deliveryDate: "2023-02-22T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Fiona",
+        email: "fiona@example.com",
+        address: "Address 68"
+      },
+      restaurant: "Cafe (SKM), Sekolah Kejuruteraan Mekanikal (tepi sungai)",
+      deliveryDate: "2023-01-14T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "George",
+        email: "george@example.com",
+        address: "Address 39"
+      },
+      restaurant: "Cafe P19, SKE",
+      deliveryDate: "2023-05-29T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Hannah",
+        email: "hannah@example.com",
+        address: "Address 82"
+      },
+      restaurant: "Cafe P03 SKE",
+      deliveryDate: "2023-03-17T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Ivy",
+        email: "ivy@example.com",
+        address: "Address 10"
+      },
+      restaurant: "Aina Cafe and Catering, T02",
+      deliveryDate: "2023-07-03T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Jack",
+        email: "jack@example.com",
+        address: "Address 89"
+      },
+      restaurant: "Embun Cafe, KTFV",
+      deliveryDate: "2023-09-20T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Alice",
+        email: "alice@example.com",
+        address: "Address 29"
+      },
+      restaurant: "Cafe, Arked Meranti",
+      deliveryDate: "2023-11-08T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Bob",
+        email: "bob@example.com",
+        address: "Address 62"
+      },
+      restaurant: "Richiamo Cafe, Arked Meranti",
+      deliveryDate: "2023-12-17T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Hannah",
+        email: "hannah@example.com",
+        address: "Address 31"
+      },
+      restaurant: "Cafe, Arked Meranti",
+      deliveryDate: "2023-10-05T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Ivy",
+        email: "ivy@example.com",
+        address: "Address 71"
+      },
+      restaurant: "The Kave, Arked Cengal",
+      deliveryDate: "2023-02-12T00:00:00.000Z"
+    },
+    {
+      user: {
+        name: "Jack",
+        email: "jack@example.com",
+        address: "Address 47"
+      },
+      restaurant: "Deen Corner, Arked Cengal",
+      deliveryDate: "2023-06-29T00:00:00.000Z"
+    }
+  ];
+
+  return completedDeliveries;
+};
+
+export default completedDeliveries;
