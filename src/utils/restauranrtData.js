7b51d344fe25b4a5e3390535bbcee1bf9bb663ea
Ishmum <ishmum20000@gmail.com>
2023-08-14 15:35:37 +0800

    Add dummy restaurntData

---
+++ src/utils/restauranrtData.js
@@ -0,0 +1,23 @@
+const restaurantData = () => {
+  const restaurants = [
+    { name: "Kafe Selasih, Arked Cengal", deliveries: 12 },
+    { name: "Deen Corner, Arked Cengal", deliveries: 8 },
+    { name: "Belanga Cafe, Arked Cengal", deliveries: 6 },
+    { name: "The Kave, Arked Cengal", deliveries: 5 },
+    { name: "PUDJ, Arked N24 dan Arked KDSE", deliveries: 9 },
+    {
+      name: "Cafe (SKM), Sekolah Kejuruteraan Mekanikal (tepi sungai)",
+      deliveries: 7,
+    },
+    { name: "Cafe P19, SKE", deliveries: 11 },
+    { name: "Cafe P03 SKE", deliveries: 6 },
+    { name: "Aina Cafe and Catering, T02", deliveries: 4 },
+    { name: "Embun Cafe, KTF", deliveries: 3 },
+    { name: "VCafe, Arked Meranti", deliveries: 7 },
+    { name: "Richiamo Cafe, Arked Meranti", deliveries: 8 },
+  ];
+
+  return restaurants;
+}
+
+export default restaurantData;
